c93d508c649f537e5bc814b9494c818e4c293fe3
牛一恒 <niuyiheng@Ians-MacBook-Pro.local>
2022-09-28 23:13:45 -0700

    feat: week3 inclass practice

--- App.js
+++ App.js
@@ -1,27 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Button, Text, SafeAreaView } from 'react-native';
 import Header from './components/Header';
 import Input from './components/Input';
+import {useState} from 'react'
 
 export default function App() {
   const name = 'UNmyWifi'
   const handleTextInput = (newText) => {
     console.log(newText)
   }
+  const [modalVisible, setModalVisible] = useState(false)
+
   return (
-    <View style={styles.container}>
-      <Header appName={name}/>
-      <Input handleTextInput={handleTextInput}/>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.topContainer}>
+        <Header appName={name}/>
+        <Button title='Add a goal' onPress={() => setModalVisible(true)}/>
+      </View>
+      <View style={styles.mainContainer}>
+        <View style={styles.mainTextContainer}>
+          <Text style={styles.mainText}>You typed...</Text>
+        </View>
+      </View>
+      <Input
+        handleTextInput={handleTextInput}
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff', 
+    justifyContent: 'center',
+  },
+  topContainer: {
+    flex: 1,
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  mainContainer: {
+    flex: 4,
+    backgroundColor: '#CEE3EF',
+    alignItems: 'center'
+  },
+  mainTextContainer: {
+    borderWidth: 1,
+    backgroundColor: '#39AFF1',
+    padding: 5,
+    borderRadius: 10,
+  },
+  mainText: {
+    color: '#fff'
+  }
 });
--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 export default function Header(props) {
   const { appName } = props
   return (
     <View>
-      <Text>{appName}</Text>
+      <Text style={styles.title}>{appName}</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 25,
+    borderWidth: 2,
+    borderColor: 'orange',
+    padding: 6
+  }
+});
+
 
